c89c96d10320512e74f0ffce0db3dacb9aef78ce
rkapka <radoslaw.kapka@gmail.com>
2025-04-09 18:07:49 +0200

    fix file name again

--- beacon-chain/operations/attestations/bitlist-error/handle.go
+++ beacon-chain/operations/attestations/bitlist-error/handle.go
@@ -39,8 +39,8 @@ func (h *BitlistErrorHandler) Handle(id attestation.Id, att ethpb.Att, seenBitli
 	h.BitlistErrCount[id] = count + 1
 	h.BitlistErrCountLock.Unlock()
 
-	attFilename := os.TempDir() + hexutil.Encode([]byte(id.String())) + "-atts-" + strconv.FormatUint(count, 10) + ".json"
-	dumpFilename := os.TempDir() + hexutil.Encode([]byte(id.String())) + "-dump-" + strconv.FormatUint(count, 10) + ".json"
+	attFilename := os.TempDir() + "/" + hexutil.Encode([]byte(id.String())) + "-atts-" + strconv.FormatUint(count, 10) + ".json"
+	dumpFilename := os.TempDir() + "/" + hexutil.Encode([]byte(id.String())) + "-dump-" + strconv.FormatUint(count, 10) + ".json"
 	log.Debugf(
 		"Found attestations with different bitlist lengths (%d and %d). Saving attestation JSON to %s and fork choice dump JSON to %s",
 		len(att.GetAggregationBits()),
