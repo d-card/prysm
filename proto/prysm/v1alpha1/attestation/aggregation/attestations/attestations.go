70daad51218bc2e4f4aee612366934a30237f485
rkapka <radoslaw.kapka@gmail.com>
2025-04-09 17:43:51 +0200

    fix logs and file names

--- proto/prysm/v1alpha1/attestation/aggregation/attestations/attestations.go
+++ proto/prysm/v1alpha1/attestation/aggregation/attestations/attestations.go
@@ -1,7 +1,6 @@
 package attestations
 
 import (
-	"github.com/google/martian/log"
 	"github.com/pkg/errors"
 	bitlist_error "github.com/prysmaticlabs/prysm/v5/beacon-chain/operations/attestations/bitlist-error"
 	"github.com/prysmaticlabs/prysm/v5/crypto/bls"
@@ -63,10 +62,10 @@ func AggregateDisjointOneBitAtts(atts []ethpb.Att, beh *bitlist_error.BitlistErr
 			if err != nil {
 				return nil, err
 			}
-			log.Debugf("Coverage length before ToBitlist64() is %d", atts[0].GetAggregationBits().Len())
-			log.Debugf("Coverage length after ToBitlist64() is %d", coverage.Len())
-			log.Debugf("Attestation length before ToBitlist64() is %d", att.GetAggregationBits().Len())
-			log.Debugf("Attestation length after ToBitlist64() is %d", bits.Len())
+			logrus.Debugf("Coverage length before ToBitlist64() is %d", atts[0].GetAggregationBits().Len())
+			logrus.Debugf("Coverage length after ToBitlist64() is %d", coverage.Len())
+			logrus.Debugf("Attestation length before ToBitlist64() is %d", att.GetAggregationBits().Len())
+			logrus.Debugf("Attestation length after ToBitlist64() is %d", bits.Len())
 			beh.Handle(id, att, coverage.ToBitlist())
 			return nil, errors.Wrap(err, "could not get aggregation bits")
 		}
