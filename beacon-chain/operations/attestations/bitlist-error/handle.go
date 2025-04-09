8e8234a070a28ec4bf06c6a71a2760d748cdabbf
rkapka <radoslaw.kapka@gmail.com>
2025-04-09 20:37:47 +0200

    add seen bitlist file

--- beacon-chain/operations/attestations/bitlist-error/handle.go
+++ beacon-chain/operations/attestations/bitlist-error/handle.go
@@ -39,15 +39,24 @@ func (h *BitlistErrorHandler) Handle(id attestation.Id, att ethpb.Att, seenBitli
 	h.BitlistErrCount[id] = count + 1
 	h.BitlistErrCountLock.Unlock()
 
+	seenFilename := os.TempDir() + "/" + hexutil.Encode([]byte(id.String())) + "-seen-" + strconv.FormatUint(count, 10) + ".json"
 	attFilename := os.TempDir() + "/" + hexutil.Encode([]byte(id.String())) + "-atts-" + strconv.FormatUint(count, 10) + ".json"
 	dumpFilename := os.TempDir() + "/" + hexutil.Encode([]byte(id.String())) + "-dump-" + strconv.FormatUint(count, 10) + ".json"
 	log.Debugf(
 		"Found attestations with different bitlist lengths (%d and %d). Saving attestation JSON to %s and fork choice dump JSON to %s",
-		len(att.GetAggregationBits()),
-		len(seenBitlist),
+		att.GetAggregationBits().Len(),
+		seenBitlist.Len(),
 		attFilename,
 		dumpFilename,
 	)
+
+	jsonSeen, marshalErr := json.Marshal(seenBitlist)
+	if marshalErr != nil {
+		log.WithError(marshalErr).Debug("Could not marshal seen bitlist into JSON")
+	}
+	if writeErr := file.WriteFile(seenFilename, jsonSeen); writeErr != nil {
+		log.WithError(writeErr).Debug("Could not write seen bitlist JSON to file")
+	}
 
 	jsonAtt, marshalErr := json.Marshal(att)
 	if marshalErr != nil {
